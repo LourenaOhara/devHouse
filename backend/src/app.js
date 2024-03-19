d190776864bfb54e228a86b00cc216625101bda2
LourenaOhara <lourena6ohara@gmail.com>
2024-03-19 18:36:43 -0300

    upload project development

---
+++ backend/src/app.js
@@ -0,0 +1,36 @@
+import express from 'express'; 
+import mongoose from 'mongoose';
+import path from 'path';
+import routes from './routes';
+
+
+class App {
+  constructor() {
+    this.server = express();
+
+    mongoose.connect('mongodb+srv://devhouse:devhouse@devhouse.axnvngy.mongodb.net/?retryWrites=true&w=majority&appName=DevHouse', {
+      //useNewUrlParser: true,
+      //useUnifiedTopology: true,
+    });
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+
+    //ao clicar na url, ele abre no navegador
+    this.server.use(
+      '/files',
+      express.static(path.resolve(__dirname, '../uploads'))
+    );
+    this.server.use(express.json());
+
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+export default new App().server;
