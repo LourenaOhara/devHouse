cd90b4adbf6b6613a46f309d335bbaf21a3903d1
LourenaOhara <lourena6ohara@gmail.com>
2024-03-21 14:01:34 -0300

    improving validations and code standards

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,33 +1,33 @@
-import express from 'express'; 
+import express from 'express';
 import mongoose from 'mongoose';
 import path from 'path';
 import cors from 'cors';
 import routes from './routes';
 
-
 class App {
   constructor() {
     this.server = express();
 
-    mongoose.connect('mongodb+srv://devhouse:devhouse@devhouse.axnvngy.mongodb.net/?retryWrites=true&w=majority&appName=DevHouse', {
-      //useNewUrlParser: true,
-      //useUnifiedTopology: true,
-    });
+    mongoose.connect(
+      'mongodb+srv://devhouse:devhouse@devhouse.axnvngy.mongodb.net/?retryWrites=true&w=majority&appName=DevHouse',
+      {
+        // useNewUrlParser: true,
+        // useUnifiedTopology: true,
+      }
+    );
 
     this.middlewares();
     this.routes();
   }
 
   middlewares() {
-
     this.server.use(cors({}));
-    //ao clicar na url, ele abre no navegador
+    // ao clicar na url, ele abre no navegador
     this.server.use(
       '/files',
       express.static(path.resolve(__dirname, '../uploads'))
     );
     this.server.use(express.json());
-
   }
 
   routes() {
--- backend/src/controllers/ReserveController.js
+++ backend/src/controllers/ReserveController.js
@@ -1,55 +1,58 @@
-import Reserve from "../models/Reserve";
-import User from "../models/User";
-import House from "../models/House";
+import Reserve from '../models/Reserve';
+import User from '../models/User';
+import House from '../models/House';
 
-class ReserveController{
-  //lista de reservas
-  async index(req, res){
+class ReserveController {
+  // lista de reservas
+  async index(req, res) {
     const { user_id } = req.headers;
 
     const reserves = await Reserve.find({ user: user_id }).populate('house');
 
     return res.json(reserves);
-
   }
-  //realizar reserva
-  async store(req,res){
+
+  // realizar reserva
+  async store(req, res) {
     const { user_id } = req.headers;
     const { house_id } = req.params;
     const { date } = req.body;
 
-    //house_id incorreto ou inexistente
+    // house_id incorreto ou inexistente
     const house = await House.findById(house_id);
-    if(!house){
-      return res.status(400).json({ error: `This house and house_id doesn't exist` });
+    if (!house) {
+      return res
+        .status(400)
+        .json({ error: `This house and house_id doesn't exist` });
     }
 
-    //status da casa é diferente de true
-    if(house.status !== true){
+    // status da casa é diferente de true
+    if (house.status !== true) {
       return res.status(400).json({ error: 'The house_id is other than true' });
     }
 
-    //user_id é igual ao house.user (é o proprietário da casa)
+    // user_id é igual ao house.user (é o proprietário da casa)
     const user = await User.findById(user_id);
-    if(String(user_id) === String(house.user)){
+    if (String(user_id) === String(house.user)) {
       return res.status(400).json({ error: 'Unauthorized' });
     }
 
     const reserve = await Reserve.create({
       user: user_id,
-      house: house_id, 
+      house: house_id,
       date,
     });
 
     const populatedReserve = await Reserve.findOne({ _id: reserve._id })
-    .populate('user')
-    .populate('house')
-    .exec();
+      .populate('user')
+      .populate('house')
+      .exec();
 
     return res.json(populatedReserve);
   }
-  //cancela reserva
-  async destroy(req, res){
+
+  // cancela reserva
+  async destroy(req, res) {
     const { reserve_id } = req.body;
 
     await Reserve.findByIdAndDelete({ _id: reserve_id });
